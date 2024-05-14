79d6b18eabbbdc541ecce6aebeb433ea46e07ece
shankar88gt <shankar.88.gt@gmail.com>
2024-05-13 22:51:52 -0500

    Feature Selection

--- CH6 - Feature Selection/Step1 - Feature Variance Module.py
+++ CH6 - Feature Selection/Step1 - Feature Variance Module.py
@@ -1,23 +1,20 @@
 ###***************************************************************************************************
 ### Step 1 - remove all features with no variance; these are like constants and doesnt add any value
-###
-### transformed data will have all columns with variance less
-### than 0.1 removed
+### transformed data will have all columns with variance less than threshold removed
 ### **************************************************************************************************
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
 from sklearn.feature_selection import VarianceThreshold
+from itertools import compress
 data = fetch_california_housing()
 X = data["data"]
 col_names = data["feature_names"]
+print(col_names)
 Y = data["target"]
 df = pd.DataFrame(data=X,columns=col_names)
-print(df.head())
-var_tresh = VarianceThreshold(threshold=0.1)
+var_tresh = VarianceThreshold(threshold=10)
 transformed_data = var_tresh.fit_transform(df)
-print(var_tresh.get_support(indices=True))
-df1 = pd.DataFrame(data=transformed_data)
+df1 = pd.DataFrame(data=transformed_data,columns=[list(compress(col_names, var_tresh.get_support(indices=False)))])
 print(df1.head())
-#print(transformed_data.columns.to_list())
 
 
--- CH6 - Feature Selection/Step2 - Univariate Feature selections.py
+++ CH6 - Feature Selection/Step2 - Univariate Feature selections.py
@@ -17,7 +17,7 @@ from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import SelectPercentile
 
 class UnivariateFeatureSelection:
-    def _init_(self,n_features,problem_type,scoring):
+    def __init__(self,n_features,problem_type,scoring):
         ### n_features : selectpercentile if float else SelectKbest
         ### Problem Type : classification or Regression
         ### scoring : scoring function
@@ -30,28 +30,53 @@ class UnivariateFeatureSelection:
         else:
             valid_scoring = {
                 "f_regression" : f_regression,
-                "mutual_info_regression" : mutual_info_regression                
+                "mutual_info_regression" : mutual_info_regression
             }
         if scoring not in valid_scoring:
             raise Exception("Invalid Scoring function")
         
         if isinstance(n_features, int):
-            self.selection = selectKbest(valid_scoring[scoring],k=n_features)
+            self.selection = SelectKBest(valid_scoring[scoring],k=n_features)
         elif isinstance(n_features, float):
             self.selection = SelectPercentile(valid_scoring[scoring],percentile=(n_features*100))
         else: raise Exception("Invalid Type of feature")
 
-        def fit(self,X,y):
-            return self.selection.fit(X,y)
+    def fit(self,X,y):
+        return self.selection.fit(X,y)
         
-        def transform(self,X):
-            return self.selection.transform(X)
+    def transform(self,X):
+        return self.selection.transform(X)
 
-        def fit_transform(self,X,y):
-            return self.selection.fit_transform(X,y)
-
+    def fit_transform(self,X,y):
+        return self.selection.fit_transform(X,y)
 
 ### Sample example 
-ufs = UnivariateFeatureSelection(n_features=0.1,problem_type='regression',scoring='f_regression')
-ufs.fit(X,y)
-ufs.transformed = ufs.transform(X)
+import pandas as pd
+import numpy as np
+from sklearn.datasets import fetch_california_housing
+from itertools import compress
+
+data = fetch_california_housing()
+X = data["data"]
+col_names = data["feature_names"]
+print(col_names)
+Y = data["target"]
+#df = pd.DataFrame(data=X,columns=col_names)
+
+ufs = UnivariateFeatureSelection(n_features=5,problem_type='regression',scoring='f_regression')
+ufs.fit(X,Y)
+scores = -np.log10(ufs.selection.pvalues_)
+print(scores)
+print(col_names)
+print(ufs.selection.get_support(False))
+
+
+import matplotlib.pyplot as plt
+X_indices = np.arange(X.shape[-1])
+plt.figure(1)
+plt.clf()
+plt.bar(X_indices - 0.05, scores, width=0.2)
+plt.title("Feature univariate score")
+plt.xlabel("Feature number")
+plt.ylabel(r"Univariate score ($-Log(p_{value})$)")
+plt.show()
